77e0b52737a6ed8f91799797fb52b6f87f68b6e9
zb666 <1360799362@qq.com>
2019-03-16 16:42:48 +0800

    Rn 美团首页 闪屏界面 以及点击跳转功能完善

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 /**
  * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
 import {AppRegistry} from 'react-native';
-import Parent from './Parent'
+import App from './App';
+import React, {Component} from 'react';
 import {name as appName} from './app.json';
-import Main from './js/Main/main.js'
-AppRegistry.registerComponent(appName, () => Main);
+import Launcher from './js/Launcher/launcher'; //-------------- ./代表的是回到上一级的目录也就MeiTuanRnPj这个文件夹之下,然后再进入到Js文件夹之下
+import {Navigator} from "react-native-deprecated-custom-components"
+
+/**
+ * abstract class  Navigator{
+ *     Object component
+ *
+ *     constuct(){
+ *         component= initialRoute()
+ *
+ *     }
+ *
+ *  abstract initialRoute(component );
+ * onDraw(){   renderScene
+ *
+ *     return Component
+ *
+ * }
+ * }
+ */
+class MeiTuan extends Component {
+    render(): React.ReactNode {
+        return <Navigator
+            initialRoute={{
+                name: "启动页",
+                component: Launcher
+            }}
+            renderScene={(router, navigator) => {
+                let Component = router.component;
+                return <Component {...router.params} navigator={navigator}/>;
+
+            }
+            }
+        />;
+    }
+
+}
+
+AppRegistry.registerComponent(appName, () => MeiTuan);
